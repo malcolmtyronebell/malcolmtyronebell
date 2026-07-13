fd7f634118e38e00005e6d43039ed255362b03fd
Malcolm <malcolmtbell@gmail.com>
2026-07-13 14:09:10 -0700

    Update documentation link in WN11-SO-000030.ps1

--- STIG's/STIG Remediations/Powershell/WN11-SO-000030.ps1
+++ STIG's/STIG Remediations/Powershell/WN11-SO-000030.ps1
@@ -12,7 +12,7 @@
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN11-SO-000030
-    Documentation   : https://stigaview.com/products/win11/v2r7WN11-SO-000030/
+    Documentation   : https://stigaview.com/products/win11/v2r8/WN11-SO-000030/
 
 .TESTED ON
     Date(s) Tested  : 
