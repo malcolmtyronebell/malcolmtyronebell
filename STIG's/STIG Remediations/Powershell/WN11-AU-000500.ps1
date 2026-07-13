85102b26aad59329117d7277fca1891574cafe7d
Malcolm <malcolmtbell@gmail.com>
2026-07-13 14:08:24 -0700

    Update documentation link for WN11-AU-000500

--- STIG's/STIG Remediations/Powershell/WN11-AU-000500.ps1
+++ STIG's/STIG Remediations/Powershell/WN11-AU-000500.ps1
@@ -12,7 +12,7 @@
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN11-AU-000500
-    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-AU-000500/
+    Documentation   : https://stigaview.com/products/win11/v2r8/WN11-AU-000500/
 
 .TESTED ON
     Date(s) Tested  : 
